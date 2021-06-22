a6350c9819edba7bc91f9f5e47079afb2c72c9fd
Mega-Charizard <81663277+Mega-Charizard@users.noreply.github.com>
2021-06-22 14:35:10 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var cat = "roar";
+var kitten = "hiss";
+var new_born_kitten = "purr"
+function Meow() {
+    console.log("new_born_kitten");
+    console.log(cat+kitten);
+    var ultimate_meow=cat.concat(kitten);
+    console.log (ultimate_meow);
+    var num_kitten=kitten.length;
+    console.log(num_kitten);
+    console.log(kitten.replace("growl","scratch"));
+    console.log(new_born_kitten.toUpperCase());
+    console.log(Math.sqrt(36));
+}
